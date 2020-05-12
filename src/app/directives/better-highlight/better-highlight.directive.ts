42fe934066660f10140ac62ec3265fde61a6f5d6
AAvilan <andres.avilan@nurun.com>
2020-05-12 11:19:58 -0400

    Using HostBinding to Bind to Host Properties - Change from using Renderer2 to @HostBinding to do the dynamic styling. With HostBinding, you remove the need to pass the DOM Element, because it will be detected by HostBinding.

--- src/app/directives/better-highlight/better-highlight.directive.ts
+++ src/app/directives/better-highlight/better-highlight.directive.ts
@@ -3,27 +3,45 @@ import {
   Renderer2,
   OnInit,
   ElementRef,
-  HostListener
+  HostListener,
+  HostBinding
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  //Host Binding uses the DOM property. please visit, https://angular.io/api/core/HostBinding.
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
+  //To see the Renderer2 information, please visit https://angular.io/api/core/Renderer2.
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+    //This initialized the background color for the element. For testing purposes, you can choose a different color to see.
+    this.backgroundColor = 'transparent';
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
+    //If you uncomment the console log, you will see that the eventData is undefined. This is used only for the signature of the interface.
     //console.log(eventData);
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+
+    //The renderer2 setStyle uses the direct DOM element and abstract from that.
+    //Also, it uses the CSS style key/value to set the style. Please visit, https://angular.io/api/core/Renderer2#setStyle.
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+
+    //This uses the HostBinding approach to do some styling.
+    this.backgroundColor = 'lightblue';
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
+    //If you uncomment the console log, you will see that the eventData is undefined. This is used only for the signature of the interface.
     //console.log(eventData);
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+
+    //This uses the HostBinding approach to do some styling.
+    this.backgroundColor = 'transparent';
   }
 }
