3d4ca94f988c7f122b5a05c0b73b13f232b3e31a
AAvilan <andres.avilan@nurun.com>
2020-05-12 11:30:10 -0400

    Binding to Directive Properties - By using Property binding into a directive, we can pass values to properties in a directive as we did for components. In this case,  we use property binding to change the values dynamically in order to choose the default and highlighted color values.

--- src/app/directives/better-highlight/better-highlight.directive.ts
+++ src/app/directives/better-highlight/better-highlight.directive.ts
@@ -4,7 +4,8 @@ import {
   OnInit,
   ElementRef,
   HostListener,
-  HostBinding
+  HostBinding,
+  Input
 } from '@angular/core';
 
 @Directive({
@@ -14,13 +15,33 @@ export class BetterHighlightDirective implements OnInit {
   //Host Binding uses the DOM property. please visit, https://angular.io/api/core/HostBinding.
   @HostBinding('style.backgroundColor') backgroundColor: string;
 
+  //Thes properties will be used with property binding to select the colors
+  @Input() defaultColor: string;
+  @Input('test') highlightColor: string;
+
   //To see the Renderer2 information, please visit https://angular.io/api/core/Renderer2.
-  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+    console.log(this.constructor.name + ' constructor called.');
+
+    //These properties are initialized with the values at the constructor. Only one time.
+    this.defaultColor = 'transparent';
+    this.highlightColor = 'lightblue';
+    console.log(this.constructor.name + ' this is the defaultColor value ' + this.defaultColor)
+    console.log(this.constructor.name + ' this is the highlightColor value ' + this.highlightColor)
+  }
 
   ngOnInit() {
+    console.log(this.constructor.name + ' ngOnInit called.');
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
+
+    //Properties initialized by ngOnInit. This will override the property binding values set in the HTML element.
+    //this.defaultColor = 'transparent';
+    //this.highlightColor = 'lightblue';
+    console.log(this.constructor.name + ' this is the defaultColor value ' + this.defaultColor)
+    console.log(this.constructor.name + ' this is the highlightColor value ' + this.highlightColor)
+
     //This initialized the background color for the element. For testing purposes, you can choose a different color to see.
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 
   @HostListener('mouseenter') mouseOver(eventData: Event) {
@@ -32,7 +53,7 @@ export class BetterHighlightDirective implements OnInit {
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'lightblue');
 
     //This uses the HostBinding approach to do some styling.
-    this.backgroundColor = 'lightblue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseLeave(eventData: Event) {
@@ -42,6 +63,6 @@ export class BetterHighlightDirective implements OnInit {
     //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
 
     //This uses the HostBinding approach to do some styling.
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
 }
